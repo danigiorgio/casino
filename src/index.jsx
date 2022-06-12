deda162bff2d8ade6e68e9dee6112768104312ba
voirs <danielagiorgioe@gmail.com>
2022-06-12 02:12:18 -0300

    Set up react-query config

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from '@/components/App';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
+
+const RootApp = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(<RootApp />);
