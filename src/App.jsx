a8a3adfa5b35451dd0d097fc04494cc8c29e9e16
voirs <danielagiorgioe@gmail.com>
2022-06-13 22:53:16 -0300

    Move App.jsx to src

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import Spinner from '@/components/Spinner';
 import './App.css';
 
 function App() {
-  const LoginView = lazy(() => import('../pages/LoginView/LoginView'));
-  const GamesView = lazy(() => import('../pages/GamesView/GamesView'));
-  const PlayView = lazy(() => import('../pages/PlayView/PlayView'));
-  const GameLauncher = lazy(() => import('../components/GameLauncher'));
-  const NotFound = lazy(() => import('../components/NotFound'));
-  const Logo = lazy(() => import('../components/Logo'));
+  const LoginView = lazy(() => import('./pages/LoginView/LoginView'));
+  const GamesView = lazy(() => import('./pages/GamesView/GamesView'));
+  const PlayView = lazy(() => import('./pages/PlayView/PlayView'));
+  const GameLauncher = lazy(() => import('./components/GameLauncher'));
+  const NotFound = lazy(() => import('./components/NotFound'));
+  const Logo = lazy(() => import('./components/Logo'));
 
   return (
     <BrowserRouter>
